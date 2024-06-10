dae09ee095b9dc4e978faf1756f21d9d582cc463
Max Greive <maxgreive@googlemail.com>
2024-06-10 17:10:47 +0200

    add assets

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  de: 'Deutsch',
+};
+
+export const ui = {
+  de: {
+    'nav.home': 'Home',
+    'nav.about': 'Über uns',
+    'nav.twitter': 'Twitter',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+} as const;
+
+export const defaultLang = 'de';
+export const showDefaultLang = false;
