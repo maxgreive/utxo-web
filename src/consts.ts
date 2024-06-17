04dca62885a4676c30722f009a19c373d214dcdd
Max Greive <maxgreive@googlemail.com>
2024-06-17 17:29:08 +0200

    update config

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const site = "https://example.com";
-export const siteTitle = "Astro Blog Starter";
-export const siteThemeColor = "#27AE60";
+export const site = "https://utxo.solutions";
+export const siteTitle = "UTXO Solutions";
+export const siteThemeColor = "#7AC6BA";
 export const pagination = {
   postsPerPage: 12,
 };
