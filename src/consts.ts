97b76dcf1b6265aa4202ebd9816ab6d457db21dd
Max Greive <maxgreive@googlemail.com>
2024-06-20 17:13:53 +0200

    fix sitemap?

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const site = "https://utxo.solutions";
+export const site = "https://utxo-solutions.netlify.app";
 export const siteTitle = "UTXO Solutions";
 export const siteThemeColor = "#7AC6BA";
 export const pagination = {
