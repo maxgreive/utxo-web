4492af4090dda52eec17faba22cb37ba77ad8dca
Max Greive <maxgreive@googlemail.com>
2024-10-27 14:48:26 +0100

    show only published blog posts

--- src/utilities/getPublishedPosts.ts
+++ src/utilities/getPublishedPosts.ts
@@ -2,7 +2,7 @@ import { type CollectionEntry, getCollection } from 'astro:content';
 import { getLocaleFromUrl } from '@i18n/utilities'
 
 export default async function getPublishedPosts(locale: string, collection: "blog" = 'blog', sort: string = 'reverseChronological') {
-  let posts = (await getCollection(collection)).filter(({ data }) => !data.secret)
+  let posts = (await getCollection(collection)).filter(({ data }) => !data.secret).filter(({ data }) => new Date(data.pubDate).getTime() < Date.now())
   if (locale !== '') posts = posts.filter(function (entry: CollectionEntry<'blog'>) { return getLocaleFromUrl(entry.slug) === locale })
   if (sort === 'reverseChronological') posts = posts.sort((a: CollectionEntry<'blog'>, b: CollectionEntry<'blog'>) =>
 
