e49963c7a8bad40d8277fe1d11d4b538b6d64ab6
Max Greive <maxgreive@googlemail.com>
2024-10-30 17:04:47 +0100

    changed build cron string

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,9 +4,8 @@ import { schedule } from '@netlify/functions'
 // This is sample build hook
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/671e401adecd37614c35b723'
 
-// Schedules the handler function to run at midnight on
-// Mondays, Wednesday, and Friday
-const handler = schedule('0 0 * * 1,3,5', async () => {
+// Schedules the handler function to run at 4 A.M. each day
+const handler = schedule('0 4 * * *', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
