7518892e3b76a42604798bdd114ee474eee16c1c
Max Greive <maxgreive@googlemail.com>
2024-12-13 08:08:00 +0100

    add event pages

--- src/header.ts
+++ src/header.ts
@@ -30,39 +30,54 @@ export const headerMenu: Record<string, Record<string, navigationItem[]>> = {
 		}]
 	},
 	de: {
-		items: [{
-			path: '/',
-			label: 'Home'
-		},
-		{
-			path: '/leistungen',
-			label: 'Leistungen',
-			children: [
-				{
-					path: '/leistungen/bitcoin-business-assesment',
-					label: 'Bitcoin Business Assesment'
-				},
-				{
-					path: '/leistungen/customer-research-sprints',
-					label: 'Customer Insight Sprints'
-				},
-				{
-					path: '/leistungen/mastering-jobs-to-be-done-online-workshop',
-					label: 'Jobs to Be Done Online Workshop'
-				},
-			]
-		},
-		{
-			path: '/bitcoin',
-			label: 'Bitcoin'
-		},
-		{
-			path: '/tools',
-			label: 'Tools'
-		},
-		{
-			path: '/blog',
-			label: 'Blog'
-		}]
+		items: [
+			{
+				path: '/',
+				label: 'Home'
+			},
+			{
+				path: '/events',
+				label: 'Events',
+				children: [
+					{
+						path: '/bitcoin-business-innovation-meetup',
+						label: 'Bitcoin Business Innovation Meetup'
+					},
+					{
+						path: '/jobs-to-be-done-meetup',
+						label: 'JTBD Meetup'
+					}
+				]
+			},
+			{
+				path: '/leistungen',
+				label: 'Leistungen',
+				children: [
+					{
+						path: '/leistungen/bitcoin-business-assesment',
+						label: 'Bitcoin Business Assesment'
+					},
+					{
+						path: '/leistungen/customer-research-sprints',
+						label: 'Customer Insight Sprints'
+					},
+					{
+						path: '/leistungen/mastering-jobs-to-be-done-online-workshop',
+						label: 'Jobs to Be Done Online Workshop'
+					},
+				]
+			},
+			{
+				path: '/bitcoin',
+				label: 'Bitcoin'
+			},
+			{
+				path: '/tools',
+				label: 'Tools'
+			},
+			{
+				path: '/blog',
+				label: 'Blog'
+			}]
 	},
 };
